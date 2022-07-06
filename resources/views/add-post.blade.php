d057fb5eb43c628f30dd93820da3946b1ad803d6
ayrinsupty <ayrinsupty@hotmail.com>
2022-07-06 13:06:45 +0600

    first commit

---
+++ resources/views/add-post.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Post</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <section style="padding-top: 60px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>Add Post</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="title">Post Title</label>
+                                    <input type="text" name="title" class="form-control" placeholder="Enter Post Title" />
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="body">Post Description</label>
+                                    <textarea name="body" class="form-control" id="" cols="30" rows="3"></textarea>
+                                </div>
+
+                                <button type="submit">Add Post</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
